dd1b44f263767d9f53d022220c1424aabdac79d2
KawaiiBunnyDev <47235190+KawaiiBunnyDev@users.noreply.github.com>
2019-01-31 22:28:10 -0600

    Create KawaiiBunnyDev.py

---
+++ KawaiiBunnyDev.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+bot=commands.Bot(command_prefix='b!')
+
+@bot.event
+async def on_ready():
+print('Logged in as:')
+print(bot.user.name)
+print(bot.user.id)
+print('-----------------')
+
+
+bot.run('NTQwNzM4MTQ0ODE1ODA4NTI3.DzVRoA.hwBmI-OJR-2gMX4yOFBsa2aKgzM')
