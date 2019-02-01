2dd2279142e443d96e5457fdd34d13beb52dc940
KawaiiBunnyDev <47235190+KawaiiBunnyDev@users.noreply.github.com>
2019-01-31 22:51:09 -0600

    Update KawaiiBunnyDev.py

--- KawaiiBunnyDev.py
+++ KawaiiBunnyDev.py
@@ -6,10 +6,10 @@ bot=commands.Bot(command_prefix='b!')
 
 @bot.event
 async def on_ready():
-print('Logged in as:')
-print(bot.user.name)
-print(bot.user.id)
-print('-----------------')
+  print('Logged in as:')
+  print(bot.user.name)
+  print(bot.user.id)
+  print('-----------------')
 
 
 bot.run('NTQwNzM4MTQ0ODE1ODA4NTI3.DzVRoA.hwBmI-OJR-2gMX4yOFBsa2aKgzM')
