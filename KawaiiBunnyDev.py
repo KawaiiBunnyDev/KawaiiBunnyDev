751aa05ba051789256cb184d008f69bcbfceca5c
KawaiiBunnyDev <47235190+KawaiiBunnyDev@users.noreply.github.com>
2019-02-01 06:39:48 -0600

    Update KawaiiBunnyDev.py

--- KawaiiBunnyDev.py
+++ KawaiiBunnyDev.py
@@ -6,10 +6,15 @@ bot=commands.Bot(command_prefix='b!')
 
 @bot.event
 async def on_ready():
-  print('Logged in as:')
-  print(bot.user.name)
-  print(bot.user.id)
-  print('-----------------')
+    print('Logged in as:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-----------------')
 
+@bot.command(pass_context=True)
+async def mute(ctx,target:discord.Member):
+	role=discord.utils.get(ctx.message.server.roles,name='Muted')
 
+    
+    
 bot.run('NTQwNzM4MTQ0ODE1ODA4NTI3.DzVRoA.hwBmI-OJR-2gMX4yOFBsa2aKgzM')
